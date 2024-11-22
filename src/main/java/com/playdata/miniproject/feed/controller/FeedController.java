dd1d928c4edd0def9400460773a70d37278dd3d5
netioz <aij0513@naver.com>
2024-11-22 14:04:36 +0900

    feed v0.2

--- src/main/java/com/playdata/miniproject/feed/controller/FeedController.java
+++ src/main/java/com/playdata/miniproject/feed/controller/FeedController.java
@@ -3,7 +3,7 @@ package com.playdata.miniproject.feed.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//
 @Controller
 @RequestMapping("/feed")
 public class FeedController {
