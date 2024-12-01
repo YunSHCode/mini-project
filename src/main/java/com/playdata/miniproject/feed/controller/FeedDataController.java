d780f31884d0b32b775d9b3e875036baca4702d8
netioz <aij0513@naver.com>
2024-12-02 02:14:57 +0900

    feed v0.5

--- src/main/java/com/playdata/miniproject/feed/controller/FeedDataController.java
+++ src/main/java/com/playdata/miniproject/feed/controller/FeedDataController.java
@@ -2,18 +2,8 @@ package com.playdata.miniproject.feed.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/resources/static/js/feed/slider.js
+++ src/main/resources/static/js/feed/slider.js
@@ -10,4 +10,16 @@ $(document).ready(function(){
         infinite: true,
     });
 
+
+    // const feedPopup = document.querySelector('.feed_popup');
+    // const floatingMenu = document.querySelector('.floating_menu');
+
+    // feedPopup.addEventListener('click', floatingMenu) => {}
+
+    $('.feed_popup').on('click', function() {
+        $(this).next('.floating_menu').toggle();
+    });
+
+
+
 });
