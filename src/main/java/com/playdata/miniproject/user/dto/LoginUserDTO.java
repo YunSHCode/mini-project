138d2ca6d3c3e78b5b5aac9015341b4027cfb563
nao0964 <caxass910@gmail.com>
2024-12-02 09:27:48 +0900

    user 파트 로그아웃, 마이페이지(매퍼 구현중) 구현했습니다.

--- src/main/java/com/playdata/miniproject/user/dto/LoginUserDTO.java
+++ src/main/java/com/playdata/miniproject/user/dto/LoginUserDTO.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.type.Alias;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Alias("logininfo")
+@Alias("loginInfo")
 public class LoginUserDTO {
     private String userId;               // 아이디
     private String userPassword;               // 비밀번호
